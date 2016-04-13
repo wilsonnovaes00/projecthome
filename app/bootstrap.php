45c00e63703216f6796e39db345f2cf88beaaa35
wilsonnovaes00 <wilsonnovaes00@hotmail.com>
2016-04-13 19:29:51 -0300

    Configuration twig

---
+++ app/bootstrap.php
@@ -0,0 +1,43 @@
+
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+
+
+$app->get('/', function() use ($app) {
+
+return $app['twig']->render('hello.twig', array() );
+
+
+
+
+});
+
+
+
+
+$app->get('/contato', function() use($app){
+
+return new Response('Página de Contato', 201);
+
+});
+
+
+
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
+
+
+
+
+$app->run();
+
--- app/index.php
+++ app/index.php
@@ -4,33 +4,18 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/bootstrap.php';
 
-require_once __DIR__.'/boostrap.php';
+
+
+
  
  
 
 
  
 
-$app = new Silex\Application();
 
-
-$app->get('/', function() use ($app) {
-
-return new Response('Home', 201);
-
-});
-
-
-$app->get('/contato', function() use($app){
-
-return new Response('Página de Contato', 201);
-
-});
-
-
-
-$app->run();
  
 
 
