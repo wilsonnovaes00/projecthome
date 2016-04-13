6175a26cb86b8fcc50d938c6850ead2d058a40c7
wilsonnovaes00 <wilsonnovaes00@hotmail.com>
2016-04-13 10:19:08 -0300

    Add Symfony Twing to composer.json

--- src/index.php
+++ src/index.php
@@ -1,10 +1,27 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 require_once __DIR__.'/../vendor/autoload.php';
 
-
-
 $app = new Silex\Application();
+
+
+$app->get('/', function() use ($app) {
+
+return new Response('Home', 201);
+
+});
+
+
+$app->get('/contato', function() use($app){
+
+return new Response('Página de Contato', 201);
+
+});
+
+
 
 $app->run();
  
